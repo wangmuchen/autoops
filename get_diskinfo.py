e7bf04d6e29a446ad46d3c2ceeb1ceade5eeafce
wangxh <wangxh@koal.com>
2022-07-05 18:26:09 +0800

    feat: get 硬盘信息

---
+++ get_diskinfo.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import setget_tab
+import libssh
+
+command = "LANG=en_US.UTF-8 lsblk|grep '^sd'"
+
+host_list = setget_tab.get_host_info()
+for i in range(len(host_list)):
+    msg = libssh.ssh_exec(command,host_list[i])
+    if msg[1] == 0:
+        print("主机%s的磁盘信息清单"%host_list[i][0])
+        print(msg[0])
